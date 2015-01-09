b69da9162f735707525b6ece4b7cad5297305043
Vivek Patel <vivek_patel@umail.ucsb.edu>
2015-01-08 17:47:46 -0800

    Added code to print date and time

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -5,8 +5,12 @@
  * @version 01/08/2015 for lab00, cs56, W15
  */
 
+import java.util.Date;
+
 public class MyFirstApp {
 	public static void main(String[] args) {
-		System.out.print("I Rule!");		
+		System.out.println("I Rule!");
+		Date date = new Date();
+		System.out.println(date.toString());		
 	}
 }
