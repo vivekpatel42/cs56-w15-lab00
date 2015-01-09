5bf9caf8fb4a5b6dc2a081495d5bc6bd86d7851f
Vivek Patel <vivek_patel@umail.ucsb.edu>
2015-01-08 17:22:50 -0800

    Added javadoc comment

---
+++ MyFirstApp.java
@@ -0,0 +1,12 @@
+/**
+ * MyFirstApp is a sample program from p 8-9 of Head First Java, 2nd edition
+ * @author Kathy Sierra and Bert Bates
+ * @author Vivek Patel
+ * @version 01/08/2015 for lab00, cs56, W15
+ */
+
+public class MyFirstApp {
+	public static void main(String[] args) {
+		System.out.print("I Rule!");		
+	}
+}
